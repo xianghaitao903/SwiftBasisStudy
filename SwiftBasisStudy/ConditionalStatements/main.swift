e4b0eb04206642ba9f0a230ef87833b5cb10ac95
xianghaitao <xiang_haitao@163.com>
2015-10-20 21:31:30 +0800

    controlFlow (for and while) and ConditionalStatement (if and switch)

---
+++ SwiftBasisStudy/ConditionalStatements/main.swift
@@ -0,0 +1,136 @@
+//
+//  main.swift
+//  ConditionalStatements
+//
+//  Created by 向海涛 on 15/10/20.
+//  Copyright © 2015年 xianghaitao. All rights reserved.
+//
+
+import Foundation
+
+//MARK:条件语句if 和 switch
+//MARK: if
+var temperatureInFahrenheit = 30
+if temperatureInFahrenheit <= 31 {
+    print("It is very cold ,consider wearing a scarf")
+}
+//MARK: if {} else {}
+temperatureInFahrenheit = 42
+if temperatureInFahrenheit <= 32 {
+    print("It is very cold, consider wearing a scraf")
+} else {
+    print("It is not that cold ,wearing a t-shirt")
+}
+
+//MARK: if {} else if  {}  else {}
+if temperatureInFahrenheit <= 32 {
+    print("It is very cold, consider wearing a scraf")
+} else if temperatureInFahrenheit > 84 {
+    print("It is really warm, do not forget to wear a sunscreen ")
+} else {
+    print("It is not that cold ,wearing a t-shirt")
+}
+
+//MARK: - switch 
+
+let someCharacter :Character = "e"
+
+switch someCharacter {
+case "a","e","i","o","u" :
+        print("\(someCharacter) is vowel")
+    
+case "b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z":
+    
+    print("\(someCharacter) is a consonant")
+    
+default:
+    print("\(someCharacter) is not a vowel or a consonant")
+    
+    
+}
+
+//MARK :case为范围 
+
+let approximateCount = 180
+
+let countedThings = "moons orbiting Saturn"
+
+var naturalCount :String
+
+switch approximateCount {
+case 0:
+    naturalCount = "no"
+case 1..<5:
+    naturalCount = "a few"
+case 5..<12:
+    naturalCount = "several"
+case 12..<100:
+    naturalCount = "dozens of"
+case 100..<1000:
+    naturalCount = "hundreds of"
+default:
+    naturalCount = "many"
+    
+}
+
+print("there are \(naturalCount) \(countedThings)")
+
+//MARK:switch 通配
+
+let point = (1,2)
+switch point {
+case (0,0):
+    print("the point \(point) is at the origin")
+case (0,_):
+    print("the point \(point) is on the y-axis ")
+case (_,0):
+    print("the point \(point) is on the x-axis")
+case(-2...2,-2...2):
+    print("(\(point.0),\(point.1)) is inside the box")
+default:
+    print("\(point) is outside of the box")
+}
+
+//MARK:let 通配 (如果case 包括所有情况，那么不需要default)
+
+let point2 = (2,0)
+
+switch point {
+case (0,let y):
+    print("\(point) is on the y-axis with a y value of \(y)")
+case (let x,0):
+    print("\(point) is on the x-axis with an x value of \(x)")
+case let (x,y):
+    print("\(point) is on somewhere else")
+    
+}
+
+//MAKE:case 后面加上where限制条件
+
+let anotherPoint = (1,-1)
+
+switch anotherPoint {
+case let (x,y) where x == y:
+    print("\(anotherPoint) is on the line x==y")
+case let(x,y) where x == -y:
+    print("\(anotherPoint) is on the line x==-y")
+case let (x,y):
+    print("\(anotherPoint) is just some arbtrary point")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ SwiftBasisStudy/ControlFlow/main.swift
@@ -0,0 +1,78 @@
+//
+//  main.swift
+//  ControlFlow
+//
+//  Created by 向海涛 on 15/10/20.
+//  Copyright © 2015年 xianghaitao. All rights reserved.
+//
+
+import Foundation
+//MARK:流程控制
+
+//MARK: for index in 1...5
+for index in 1...5 {
+    print("\(index) times 5 is \(index * 5)")
+}
+
+//MARK: 不需要循环范围里面的值， 使用 _ 代替
+
+let base = 3
+
+let power = 10
+
+var answer = 1
+
+for _ in 1...power {
+    answer *= base
+}
+
+print("\(base) to the power of \(power) is \(answer)")
+
+//MARK:传统的循环
+for var index = 0; index < 3;index++ {
+    print("index is \(index)")
+}
+
+var index :Int
+for index = 0;index < 3;++index {
+    print("index is \(index)")
+}
+
+print("The loop statements were execute \(index) times")
+
+//MARK: - while Loops
+
+let finalSquare = 25
+var board = [Int](count: finalSquare + 1, repeatedValue: 0)
+
+board[03] = +08
+board[06] = +11
+board[09] = +09
+board[10] = +02
+
+board[14] = -10
+board[19] = -11
+board[22] = -02
+board[24] = -08
+
+var square = 0
+var diceRoll = 0
+while square < finalSquare {
+    if ++square == 7 {square = 1}
+    square += diceRoll
+    if square < board.count {
+        square += board[square]
+    }
+    
+}
+
+print("game over")
+//MARK: repeat { ...} while
+repeat {
+    square += board[square]
+    if ++square == 7 {square = 1}
+    square += diceRoll
+} while square < finalSquare
+
+print("game over")
+
