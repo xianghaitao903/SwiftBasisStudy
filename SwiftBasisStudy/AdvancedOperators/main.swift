1611e582c81318035ed0dc0dfd89ed2de76bcd33
xianghaitao <xiang_haitao@163.com>
2015-11-26 22:02:15 +0800

    using swift with cocoa and Objective-C

---
+++ SwiftBasisStudy/AdvancedOperators/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  AdvancedOperators
+//
+//  Created by 向海涛 on 15/11/26.
+//  Copyright © 2015年 xianghaitao. All rights reserved.
+//
+
+import Foundation
+//MARK: bitwise NOT Operators
+
+let initialBit: UInt8 = 0b00001111
+let invertedBit = ~initialBit  //invertedBit is 0b11110000
+print(invertedBit)
+
+
---
+++ UsingSwiftWithCocoaAndOC/UsingSwiftWithCocoaAndOC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UsingSwiftWithCocoaAndOC
+//
+//  Created by 向海涛 on 15/11/26.
+//  Copyright © 2015年 xianghaitao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
